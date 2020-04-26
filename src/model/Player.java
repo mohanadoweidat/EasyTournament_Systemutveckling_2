9ca7600fa8988ed4d0f88fb66bacd8ec2dd07747
AndreasvonU <andreasvonu@gmail.com>
2020-04-26 18:00:05 +0200

    fixed columns

---
+++ src/model/Player.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Player {
+    private String name;
+
+    public Player(String name){
+        this.name=name;
+    }
+    public Player(){
+        this.name="";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
