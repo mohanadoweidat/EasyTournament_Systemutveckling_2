961712456df34e88c7062305065424589f030c9b
AndreasvonU <andreasvonu@gmail.com>
2020-05-03 20:32:12 +0200

    eight team imple

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -64,6 +64,59 @@ public class Controller {
     @FXML
     private ChoiceBox<AmountOfTeams> teamsBox = new ChoiceBox();
 
+
+
+    @FXML
+    private TextField tfTeam1 = new TextField();
+    @FXML
+    private TextField tfTeam2 = new TextField();
+    @FXML
+    private TextField tfTeam3 = new TextField();
+    @FXML
+    private TextField tfTeam4 = new TextField();
+    @FXML
+    private TextField tfTeam5 = new TextField();
+    @FXML
+    private TextField tfTeam6 = new TextField();
+    @FXML
+    private TextField tfTeam7 = new TextField();
+    @FXML
+    private TextField tfTeam8 = new TextField();
+    @FXML
+    private TextField tfSemifinal1Home = new TextField();
+    @FXML
+    private TextField tfSemifinal1Away = new TextField();
+    @FXML
+    private TextField tfSemifinal2Home = new TextField();
+    @FXML
+    private TextField tfSemifinal2Away = new TextField();
+    @FXML
+    private TextField tfFinalHome = new TextField();
+    @FXML
+    private TextField tfFinalAway = new TextField();
+    @FXML
+    private Label lblTeam1 = new Label();
+    @FXML
+    private Label lblSemifinal1 = new Label();
+    @FXML
+    private Label lblSemifinal1Away = new Label();
+    @FXML
+    private Label lblSemifinal2Home = new Label();
+    @FXML
+    private Label lblSemifinal2Away = new Label();
+    @FXML
+    private Label lblFinalHome = new Label();
+    @FXML
+    private Label lblFinalAway = new Label();
+    @FXML
+    private Label lblWinner = new Label();
+
+
+
+
+
+
+
     public Controller() {
         setEditable();
     }
@@ -174,6 +227,136 @@ public class Controller {
                 tblTeams.setItems(getPlayer(playerSave));
                 column.setCellValueFactory(new PropertyValueFactory<>("name"));
                 break;
+        }
+    }
+
+    public void playoffsResult(){
+        playoffsController.results();
+    }
+
+    public void results() {
+        boolean winner = true;
+
+        int score1 = Integer.parseInt(tfTeam1.getText());
+        int score2 = Integer.parseInt(tfTeam2.getText());
+
+        int score3 = Integer.parseInt(tfTeam3.getText());
+        int score4 = Integer.parseInt(tfTeam4.getText());
+
+//        int score5 = Integer.parseInt(tfTeam5.getText());
+//        int score6 = Integer.parseInt(tfTeam6.getText());
+//
+//        int score7 = Integer.parseInt(tfTeam7.getText());
+//        int score8 = Integer.parseInt(tfTeam8.getText());
+//
+//        int score9 = Integer.parseInt(tfSemifinal1Home.getText());
+//        int score10 = Integer.parseInt(tfSemifinal1Away.getText());
+//
+//        int score11 = Integer.parseInt(tfSemifinal2Home.getText());
+//        int score12 = Integer.parseInt(tfSemifinal2Away.getText());
+//
+//        int score13 = Integer.parseInt(tfFinalHome.getText());
+//        int score14 = Integer.parseInt(tfFinalAway.getText());
+
+
+        if (score1 > score2) {
+                tfSemifinal1Home.setPromptText("Team 1");
+                lblSemifinal1.setText("Team 1");
+            } else if (score1 < score2){
+                tfSemifinal1Home.setPromptText("Team 2");
+                lblSemifinal1.setText("Team 2");
+            }
+            if (score3 > score4){
+                tfSemifinal1Away.setPromptText("Team 3");
+                lblSemifinal1Away.setText("Team 3");
+            }else {
+                tfSemifinal1Away.setPromptText("Team 4");
+                lblSemifinal1Away.setText("Team 4");
+            }
+
+    }
+    public void gameOne(){
+        int score1 = Integer.parseInt(tfTeam1.getText());
+        int score2 = Integer.parseInt(tfTeam2.getText());
+        if (score1 > score2) {
+            tfSemifinal1Home.setPromptText("Team 1");
+            lblSemifinal1.setText("Team 1");
+        } else if (score1 < score2){
+            tfSemifinal1Home.setPromptText("Team 2");
+            lblSemifinal1.setText("Team 2");
+        }
+    }
+
+    public void gameTwo(){
+        int score3 = Integer.parseInt(tfTeam3.getText());
+        int score4 = Integer.parseInt(tfTeam4.getText());
+        if (score3 > score4){
+            tfSemifinal1Away.setPromptText("Team 3");
+            lblSemifinal1Away.setText("Team 3");
+        }else {
+            tfSemifinal1Away.setPromptText("Team 4");
+            lblSemifinal1Away.setText("Team 4");
+        }
+    }
+
+    public void gameThree(){
+        int score5 = Integer.parseInt(tfTeam5.getText());
+        int score6 = Integer.parseInt(tfTeam6.getText());
+
+        if (score5 > score6){
+            tfSemifinal2Home.setPromptText("Team 5");
+            lblSemifinal2Home.setText("Team 5");
+        } else{
+            tfSemifinal2Home.setPromptText("Team 6");
+            lblSemifinal2Home.setText("Team 6");
+        }
+    }
+
+    public void gameFour(){
+        int score7 = Integer.parseInt(tfTeam7.getText());
+        int score8 = Integer.parseInt(tfTeam8.getText());
+
+        if (score7 > score8){
+            tfSemifinal2Away.setPromptText("Team 7");
+            lblSemifinal2Away.setText("Team 7");
+        } else{
+            tfSemifinal2Away.setPromptText("Team 8");
+            lblSemifinal2Away.setText("Team 8");
+        }
+    }
+    public void gameFive(){
+        int score9 = Integer.parseInt(tfSemifinal1Home.getText());
+        int score10 = Integer.parseInt(tfSemifinal1Away.getText());
+
+        if (score9 > score10){
+            lblFinalHome.setText(lblSemifinal1.getText());
+            tfFinalHome.setPromptText(lblSemifinal1.getText());
+        } else {
+            lblFinalHome.setText(lblSemifinal1Away.getText());
+            tfFinalHome.setPromptText(lblSemifinal1Away.getText());
+        }
+    }
+    public void gameSix(){
+        int score11 = Integer.parseInt(tfSemifinal2Home.getText());
+        int score12 = Integer.parseInt(tfSemifinal2Away.getText());
+
+        if (score11 > score12){
+            lblFinalHome.setText(lblSemifinal2Home.getText());
+            tfFinalHome.setPromptText(lblSemifinal2Home.getText());
+        } else {
+            lblFinalAway.setText(lblSemifinal2Away.getText());
+            tfFinalAway.setPromptText(lblSemifinal2Away.getText());
+        }
+    }
+
+    public void gameSeven(){
+        int score13 = Integer.parseInt(tfFinalHome.getText());
+        int score14 = Integer.parseInt(tfFinalAway.getText());
+
+        if (score13 > score14){
+            lblWinner.setText(lblFinalHome.getText());
+        } else {
+            lblWinner.setText(lblFinalAway.getText());
         }
     }
 
--- src/controller/PlayoffsController.java
+++ src/controller/PlayoffsController.java
@@ -6,14 +6,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.*;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class PlayoffsController {
     private Controller controller;
     private AmountOfTeams amountOfTeams;
+
+    @FXML
+    private TextField tfTeam1 = new TextField();
+    @FXML
+    private TextField tfTeam2 = new TextField();
+    @FXML
+    private Label lblTeam1 = new Label();
+    @FXML
+    private Label lblSemifinal1 = new Label();
+
+    @FXML
+    private TextField tfSemifinal1Home = new TextField();
+
+
 
     public PlayoffsController(Controller controller) {
         this.controller = controller;
@@ -26,18 +43,25 @@ public class PlayoffsController {
         switch (selectedItem) {
             case Three:
                 loader.setLocation(getClass().getResource("../view/ThreeTeamsPlayoff.fxml"));
+                break;
             case Four:
                 loader.setLocation(getClass().getResource("../view/fourTeamsPlayoff.fxml"));
+                break;
             case Five:
                 loader.setLocation(getClass().getResource("../view/FiveTeamsPlayoff.fxml"));
+                break;
             case Six:
                 loader.setLocation(getClass().getResource("../view/SixTeamsPlayoff.fxml"));
+                break;
             case Seven:
                 loader.setLocation(getClass().getResource("../view/SevenTeamsPlayoff.fxml"));
+                break;
             case Eight:
                 loader.setLocation(getClass().getResource("../view/EightTeamPlayoffs.fxml"));
+                break;
             case Nine:
                 loader.setLocation(getClass().getResource("../view/NineTeamsPlayoff.fxml"));
+                break;
             case Ten:
                 loader.setLocation(getClass().getResource("../view/TenTeamsPlayoff.fxml"));
                 break;
@@ -48,5 +72,24 @@ public class PlayoffsController {
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(playerScene);
         window.show();
+    }
+
+    public void results() {
+        boolean winner = true;
+        try {
+            int score1 = Integer.parseInt(tfTeam1.getText());
+            int score2 = Integer.parseInt(tfTeam2.getText());
+            System.out.println(score1+ ", " + score2);
+
+            if (score1 > score2) {
+                tfSemifinal1Home.setPromptText("Team 1");
+                lblSemifinal1.setText("Team 1");
+            } else if (score1 < score2) {
+                tfSemifinal1Home.setPromptText("Team 2");
+                lblSemifinal1.setText("Team 2");
+            }
+        }catch(NumberFormatException ex){ // handle your exception
+
+        }
     }
 }
