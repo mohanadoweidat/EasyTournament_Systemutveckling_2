491c352f328da0f31bfbf73a13e4b231b09a8d52
TranAnnie <77334244+TranAnnie@users.noreply.github.com>
2022-01-20 19:12:27 +0100

    commit

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,7 +7,7 @@ import model.*;
 /**
  * @author Andreas von Uthmann, Carl Hägred, Gustav Edén, Joel Svensson
  */
-public class Main extends Application { //test
+public class Main extends Application { //testMain
 
     @Override
     public void start(Stage primaryStage) {
